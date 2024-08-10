2f55b72c97523479ffc441e89b6b76c1e7261be1
Trizah75 <trizahdanson75@gmail.com>
2024-08-10 10:37:53 +0000

    updated index.html and index.js

--- src/index.js
+++ src/index.js
@@ -18,15 +18,17 @@ function displayTemperature(response) {
   temperatureElement.innerHTML = temperature;
 }
 
-function search(event) {
-  event.preventDefault();
-  let searchInputElement = document.querySelector("#search-input");
-  let city = searchInputElement.value;
-
+function searchCity(city) {
   let apiKey = "b2a5adcct04b33178913oc335f405433";
   let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
 
   axios.get(apiUrl).then(displayTemperature);
+}
+
+function search(event) {
+  event.preventDefault();
+  let searchInputElement = document.querySelector("#search-input");
+  let city = searchInputElement.value;
 }
 
 function formatDate(date) {
@@ -63,3 +65,5 @@ let currentDateELement = document.querySelector("#current-date");
 let currentDate = new Date();
 
 currentDateELement.innerHTML = formatDate(currentDate);
+
+searchCity("paris");
